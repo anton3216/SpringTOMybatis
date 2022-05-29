09fbdfc8c9bcb9fad103b60f54cbf86c9b304adc
Dahuang <anton3216@163.com>
2022-05-29 16:10:44 +0800

    ssm整合

--- src/main/java/org/qingqiao/controller/StudentController.java
+++ src/main/java/org/qingqiao/controller/StudentController.java
@@ -2,17 +2,32 @@ package org.qingqiao.controller;
 
 import org.qingqiao.bean.Student;
 import org.qingqiao.mapper.StudentMapper;
+import org.qingqiao.service.StudentService;
 import org.qingqiao.service.StudentServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 
+@Controller
+@RequestMapping("stu")
 public class StudentController {
+    @Autowired
+    private StudentService ss;
 
-    public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        StudentServiceImpl studentService = context.getBean(StudentServiceImpl.class);
-        List<Student> students = studentService.queryAll();
-        System.out.println(students);
+    @RequestMapping("queryAll")
+    public String queryAll(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<Student> students = ss.queryAll();
+        req.setAttribute("list",students);
+        return "list";
     }
 }
